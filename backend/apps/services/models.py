219f86af462bc20523364b1389c2c0681da371a1
Cedric <Ced_j_email@hotmail.com>
2026-05-13 17:16:40 -0400

    Create Service model

--- backend/apps/services/models.py
+++ backend/apps/services/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    duration_minutes = models.PositiveIntegerField(default=30)
+
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
